7922482548382fe726620ceb876b0a5a979db464
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-11-03 12:00:41 -0500

    adding routes for signup and jobs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'heroes', component: HeroesComponent }
+  { path: 'signup', component: SignUpComponent },
+  { path: 'jobs', component: JobsComponent }
 ];
 
 @NgModule({
